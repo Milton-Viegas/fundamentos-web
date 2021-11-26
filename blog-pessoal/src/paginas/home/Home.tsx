e1cc76d8d315a0f55fbb0ca21e5aa226614a39a4
Viegas <milton.viegas.br@gmail.com>
2021-11-26 12:49:20 -0300

    add react on project

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https:www.teahub.io/photos/full/321-3216806_papel-de-parede-tela-de-fundo-imagem-pantera.jpg"
+            alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );   
+}
+
+export default Home;
